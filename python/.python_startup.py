ad8d0ad3e88bf8ee68bccd7defab6835143a571a
dankmachine <williamjow67@gmail.com>
2018-02-23 00:01:45 -0800

    updated pythonstartup to delete history upon exiting

--- python/.python_startup.py
+++ python/.python_startup.py
@@ -1,5 +1,17 @@
 import os
 import numpy as np
 import math
+import atexit
+import readline
 
 clear = lambda : os.system('clear') or None
+
+# Don't save history
+readline_history_file = os.path.join(os.path.expanduser('~'), '.python_history')
+try:
+    readline.read_history_file(readline_history_file)
+except IOError:
+    pass
+
+readline.set_history_length(0)
+atexit.register(readline.write_history_file, readline_history_file)
