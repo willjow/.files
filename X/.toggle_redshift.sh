5d93132a7c931b7ddaa4b73f629183be43adca01
dankmachine <williamjow67@gmail.com>
2019-05-15 18:12:45 -0700

    added a third color temperature for redshift toggling

--- X/.toggle_redshift.sh
+++ X/.toggle_redshift.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-TOGGLE=$HOME/.redshift_indicator
-TEMPERATURE=3500
+MID_INDICATOR=$HOME/.toggle_redshift_indicator_mid
+LOW_INDICATOR=$HOME/.toggle_redshift_indicator_low
+MID_TEMP=5000
+LOW_TEMP=3500
 
-if [ ! -e $TOGGLE ]; then
-    touch $TOGGLE
-    redshift -P -O $TEMPERATURE
-else
-    rm $TOGGLE
+if [ ! -e $MID_INDICATOR ] && [ ! -e $LOW_INDICATOR ]; then
+    touch $MID_INDICATOR
+    redshift -P -O $MID_TEMP
+
+elif [ -e $MID_INDICATOR ]; then
+    rm $MID_INDICATOR
+    touch $LOW_INDICATOR
+    redshift -P -O $LOW_TEMP
+
+elif [ -e $LOW_INDICATOR ]; then
+    rm $LOW_INDICATOR
     redshift -x
 fi
