8934c9e1d2669cc067a56eec8b51245220a20139
dankmachine <williamjow67@gmail.com>
2019-05-21 20:16:52 -0700

    added another level for redshift toggling; updated deprecated options in i3 config; removed exit 33 in i3blocks temperatuer script

--- X/.toggle_redshift.sh
+++ X/.toggle_redshift.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
-MID_INDICATOR=$HOME/.toggle_redshift_indicator_mid
-LOW_INDICATOR=$HOME/.toggle_redshift_indicator_low
-MID_TEMP=5000
-LOW_TEMP=3500
+DIM1_INDICATOR=$HOME/.toggle_redshift_indicator_dim1
+DIM2_INDICATOR=$HOME/.toggle_redshift_indicator_dim2
+DIM3_INDICATOR=$HOME/.toggle_redshift_indicator_dim3
 
-if [ ! -e $MID_INDICATOR ] && [ ! -e $LOW_INDICATOR ]; then
-    touch $MID_INDICATOR
-    redshift -P -O $MID_TEMP
+DIM1_GAMMA='1:1:1'
+DIM1_TEMP=5000
 
-elif [ -e $MID_INDICATOR ]; then
-    rm $MID_INDICATOR
-    touch $LOW_INDICATOR
-    redshift -P -O $LOW_TEMP
+DIM2_GAMMA='1:1:1'
+DIM2_TEMP=4200
 
-elif [ -e $LOW_INDICATOR ]; then
-    rm $LOW_INDICATOR
+DIM3_GAMMA='1:1:1'
+DIM3_TEMP=3500
+
+if [ ! -e $DIM1_INDICATOR ] && [ ! -e $DIM2_INDICATOR ] && [ ! -e $DIM3_INDICATOR ]; then
+    touch $DIM1_INDICATOR
+    redshift -P -g $DIM1_GAMMA -O $DIM1_TEMP
+
+elif [ -e $DIM1_INDICATOR ]; then
+    rm $DIM1_INDICATOR
+    touch $DIM2_INDICATOR
+    redshift -P -g $DIM2_GAMMA -O $DIM2_TEMP
+
+elif [ -e $DIM2_INDICATOR ]; then
+    rm $DIM2_INDICATOR
+    touch $DIM3_INDICATOR
+    redshift -P -g $DIM3_GAMMA -O $DIM3_TEMP
+
+elif [ -e $DIM3_INDICATOR ]; then
+    rm $DIM3_INDICATOR
     redshift -x
 fi
