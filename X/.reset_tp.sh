f17e9db5bc77c34eacaab5077156fad57d8d03a6
willjow <williamjow67@gmail.com>
2019-07-21 12:53:51 -0700

    updated power usage to have a higher warning threshold and changed it to turn cyan when done charging; added resume service to reset the trackpoint/pad settings

---
+++ X/.reset_tp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+trackpoint="TPPS/2 IBM TrackPoint" 
+touchpad="Synaptics TM3053-004" 
+
+# trackpoint stuff
+tposet() { xinput set-prop "${trackpoint}" "$@"; }
+tpaset() { xinput set-prop "${touchpad}" "$@"; }
+
+tposet "libinput Accel Speed" 0
+tposet "libinput Accel Profile Enabled" 0, 1
+tpaset "libinput Tapping Enabled" 1
+
+# disable touchpad and buttons
+xinput enable "${touchpad}"
