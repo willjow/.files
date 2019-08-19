5340c5f76ad31ef4b14d33376252abb8216a921a
willjow <williamjow67@gmail.com>
2019-08-19 15:29:56 -0700

    fixed bug brightness adjusting wrapping below due to negative index

--- X/.adjust_backlight.sh
+++ X/.adjust_backlight.sh
@@ -2,13 +2,17 @@ cmd=$1
 pwr_file="/sys/class/backlight/intel_backlight/brightness"
 pwr=$(cat ${pwr_file})
 
+min_pwr=1
+max_pwr=4794
+lo_steps=(1 46 91 181 271 406 586)
+hi_steps=$(seq 849 263 4794)
+steps=(${lo_steps[@]} ${hi_steps[@]})
+
+
 set_pwr() {
   echo $1 > ${pwr_file}
 }
 
-lo_steps=(1 46 91 181 271 406 586)
-hi_steps=$(seq 849 263 4794)
-steps=(${lo_steps[*]} ${hi_steps[*]})
 
 i=0
 while (( ${pwr} > ${steps[$i]} )); do
@@ -16,12 +20,16 @@ while (( ${pwr} > ${steps[$i]} )); do
 done
 
 if [ ${cmd} = "inc" ]; then
-  if (( $i < 4794 )); then
+  if (( $i < ${#steps[@]} )); then
     set_pwr ${steps[$(($i + 1))]}
+  else
+    set_pwr ${max_pwr}
   fi
 elif  [ ${cmd} = "dec" ]; then
   if (( $i > 0 )); then
     set_pwr ${steps[$(($i - 1))]}
+  else
+    set_pwr ${min_pwr}
   fi
 fi
 
