af8bfc03032f73fdb980ce8df904e3c4717c25e3
willjow <williamjow67@gmail.com>
2019-08-19 03:14:24 -0700

    wrote script to allow for increased granularity with adjusting backlight

---
+++ X/.adjust_backlight.sh
@@ -0,0 +1,30 @@
+cmd=$1
+pwr=$(cat /sys/class/backlight/intel_backlight/brightness)
+
+if [ ${cmd} = "inc" ]; then
+  if [ ${pwr} -le 1 ]; then
+    xbacklight -set 1
+  elif [ ${pwr} -lt 48 ]; then
+    xbacklight -set 2
+  elif [ ${pwr} -lt 96 ]; then
+    xbacklight -set 4
+  elif [ ${pwr} -lt 192 ]; then
+    xbacklight -set 6
+  else
+    xbacklight -inc 6
+  fi
+elif  [ ${cmd} = "dec" ]; then
+  if [ ${pwr} -le 48 ]; then
+    xbacklight -set 0.02
+  elif [ ${pwr} -le 96 ]; then
+    xbacklight -set 1
+  elif [ ${pwr} -le 192 ]; then
+    xbacklight -set 2
+  elif [ ${pwr} -le 288 ]; then
+    xbacklight -set 4
+  elif [ ${pwr}-le 600 ]; then
+    xbacklight -set 6
+  else
+    xbacklight -dec 6
+  fi
+fi
