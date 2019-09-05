8f3737dce3b8d33eaa68fdf5261b17645b789604
willjow <williamjow67@gmail.com>
2019-09-05 16:36:58 -0700

    adjust backlight levels again

--- X/.adjust_backlight.sh
+++ X/.adjust_backlight.sh
@@ -5,8 +5,8 @@ pwr=$(cat ${pwr_file})
 
 min_pwr=1
 max_pwr=4794
-lo_steps=(1 46 91 181 271 406 541 721 901 1126 1351)
-hi_steps=$(seq 1626 288 4794)
+lo_steps=(1 23 46 69 91 136 181 226 271 361 451 586 721 901 1081 1306)
+hi_steps=$(seq 1527 297 4794)
 steps=(${lo_steps[@]} ${hi_steps[@]})
 
 
