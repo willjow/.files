fb578d7819e7b47859ee6da53176d72d0042e559
willjow <williamjow67@gmail.com>
2021-12-23 00:11:13 -0800

    Add brightness step for off (0)

--- X/.adjust_backlight.sh
+++ X/.adjust_backlight.sh
@@ -3,9 +3,9 @@ cmd=$1
 pwr_file="/sys/class/backlight/intel_backlight/brightness"
 pwr=$(cat ${pwr_file})
 
-min_pwr=1
+min_pwr=0
 max_pwr=4794
-lo_steps=(1 23 46 69 91 136 181 226 271 361 451 586 721 901 1081 1306)
+lo_steps=(0 1 23 46 69 91 136 181 226 271 361 451 586 721 901 1081 1306)
 hi_steps=$(seq 1527 297 4794)
 steps=(${lo_steps[@]} ${hi_steps[@]})
 
