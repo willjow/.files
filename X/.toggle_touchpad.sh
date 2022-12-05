e46528806e5683ccfa7879ea4b67bdb784fcf1f7
willjow <williamjow67@gmail.com>
2022-12-05 01:43:24 -0800

    Removed unused stuff

--- X/.toggle_touchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-device="Synaptics TM3053-004"
-state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
-
-if [ $state == '1' ]; then
-  xinput disable "$device"
-else
-  xinput enable "$device"
-fi
--- X/.xcalib.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xcalib ~/.ThinkPad_T450s_FHD_LP140WF3_SPD1_01.icc
