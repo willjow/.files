8400abdc8910d49c4c28d8c88aff245c84319c8e
willjow <williamjow67@gmail.com>
2022-12-19 15:47:56 -0800

    Update touchpad settings

--- X/.reset_tp.sh
+++ X/.reset_tp.sh
@@ -10,10 +10,15 @@ xinput enable "${touchpad}"
 tposet() { xinput set-prop "${trackpoint}" "$@"; }
 tpaset() { xinput set-prop "${touchpad}" "$@"; }
 
+# trackpoint
 tposet "libinput Accel Speed" 0.50  # in [-1, 1]
 tposet "libinput Accel Profile Enabled" 0, 1  # adaptive, flat
+
+# touchpad
 tpaset "libinput Tapping Enabled" 0
 tpaset "libinput Disable While Typing Enabled" 0
+tpaset "libinput Accel Speed" -0.11  # in [-1, 1]
+tpaset "libinput Accel Profile Enabled" 1, 0  # adaptive, flat
 
 # disable touchpad and buttons
 xinput disable "${touchpad}"
