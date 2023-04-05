e870733dbb7198b52b7b19dd5104ac302efec9be
willjow <williamjow67@gmail.com>
2023-04-04 21:05:00 -0700

    Reset trackpoint when toggling touchpad

--- X/.toggle_touchpad.sh
+++ X/.toggle_touchpad.sh
@@ -4,6 +4,7 @@ state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
 
 if [ $state == '1' ]; then
   xinput disable "$device"
+  . $HOME/.reset_tp.sh
 else
   xinput enable "$device"
 fi
