078c9b97654a3b97a24e2708270fe2e3730be6cf
willjow <williamjow67@gmail.com>
2023-09-23 15:39:42 -0700

    Tweak trackpoint/touchpad sensitivities

--- X/.reset_tp.sh
+++ X/.reset_tp.sh
@@ -11,13 +11,13 @@ tposet() { xinput set-prop "${trackpoint}" "$@"; }
 tpaset() { xinput set-prop "${touchpad}" "$@"; }
 
 # trackpoint
-tposet "libinput Accel Speed" 0.45  # in [-1, 1]
+tposet "libinput Accel Speed" 0.55  # in [-1, 1]
 tposet "libinput Accel Profile Enabled" 0, 1  # adaptive, flat
 
 # touchpad
 tpaset "libinput Tapping Enabled" 0
 tpaset "libinput Disable While Typing Enabled" 0
-tpaset "libinput Accel Speed" -0.11  # in [-1, 1]
+tpaset "libinput Accel Speed" -0.09  # in [-1, 1]
 tpaset "libinput Accel Profile Enabled" 1, 0  # adaptive, flat
 
 # disable touchpad and buttons
