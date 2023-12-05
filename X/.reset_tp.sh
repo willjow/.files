b44264e3abaf14c1214c67d879d396f75b872942
willjow <williamjow67@gmail.com>
2023-12-05 15:38:47 -0800

    Update trackpoint config * libinput:   - Scale using a transformation matrix instead of increasing   the flat accel factor     - Using a positive flat accel value skips more pixels in comparison       to scaling with a transformation matrix   - Increase scrolling speed * udev:   - Increase sensitivity to max (255) from default (200)   - Decrease speed from default (97) to 42     - This value mainly seems to affect fine movements... past a certain       pressure threshold it doesn't appear to make much of a difference

--- X/.reset_tp.sh
+++ X/.reset_tp.sh
@@ -11,14 +11,16 @@ tposet() { xinput set-prop "${trackpoint}" "$@"; }
 tpaset() { xinput set-prop "${touchpad}" "$@"; }
 
 # trackpoint
-tposet "libinput Accel Speed" 0.55  # in [-1, 1]
-tposet "libinput Accel Profile Enabled" 0, 1  # adaptive, flat
+tposet "Coordinate Transformation Matrix" 1.8, 0, 0, 0, 1.8, 0, 0, 0, 1
+tposet "libinput Accel Speed" 0  # in [-1, 1]
+tposet "libinput Accel Profile Enabled" 0, 1, 0  # adaptive, flat, custom
+tposet "libinput Scrolling Pixel Distance" 10  # 10 seems to be the min
 
 # touchpad
 tpaset "libinput Tapping Enabled" 0
 tpaset "libinput Disable While Typing Enabled" 0
 tpaset "libinput Accel Speed" -0.09  # in [-1, 1]
-tpaset "libinput Accel Profile Enabled" 1, 0  # adaptive, flat
+tpaset "libinput Accel Profile Enabled" 1, 0, 0  # adaptive, flat, custom
 
 # disable touchpad and buttons
 xinput disable "${touchpad}"
